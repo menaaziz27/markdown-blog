809f653b578026cbe72794845a0df46d19ecf6ce
menaaziz27 <menaaziz27@gmail.com>
2021-12-07 23:25:32 +0200

    fortify

--- blog-client/src/main.js
+++ blog-client/src/main.js
@@ -4,6 +4,6 @@ import './assets/app.css';
 import router from './router';
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://127.0.0.1:8000';
+axios.defaults.baseURL = 'http://127.0.0.1:8001';
 
 createApp(App).use(router).mount('#app');
